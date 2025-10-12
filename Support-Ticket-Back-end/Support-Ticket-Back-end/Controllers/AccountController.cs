a690fe7a9da44fee32485f0c41d88ff9cc0b97d4
hmmam-ziad <hmmamxxc@gmail.com>
2025-10-12 22:30:21 +0300

    Create Account Controller

---
+++ Support-Ticket-Back-end/Support-Ticket-Back-end/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Support_Ticket_Back_end.Models;
+
+namespace Support_Ticket_Back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IConfiguration _config;
+        public AccountController(UserManager<UserApp> userManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _config = config;
+        }
+    }
+}
